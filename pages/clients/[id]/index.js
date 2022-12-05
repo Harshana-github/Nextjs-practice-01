1ea638397a8e6815ebf4e206daa414b186cb7a70
Harshana Lakmal <harshanalakmal503@gmail.com>
2022-12-05 12:06:46 +0530

    Navigating-Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,18 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 function ClientsProjectsPage() {
+  const router = useRouter()
+  function loadProjectHandler() {
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'har', clientprojectid: 'projecta' }
+    })
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
